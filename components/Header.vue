5ee0403c54d67b99152b02be3476612dec34405b
sohei24 <sohei24.eng@gmail.com>
2021-10-27 22:07:00 +0900

    first-commit

---
+++ components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="header">
+    <div class="header_icon">
+      <span class="header_icon__line--top"></span>
+      <span class="header_icon__line--middle"></span>
+      <span class="header_icon__line--bottom"></span>
+    </div>
+    <div class="header_icon--text">Rese</div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+}
+
+.header_icon {
+  display: inline-block;
+  width: 36px;
+  height: 32px;
+  cursor: pointer;
+  position: relative;
+  left: 20px;
+  top: 20px;
+  background-color: blue;
+  border-radius: 4px;
+  padding: 2px;
+}
+.header_icon__line--top,
+.header_icon__line--middle,
+.header_icon__line--bottom {
+  display: inline-block;
+  width: 100%;
+  height: 3px;
+  background-color: white;
+  position: absolute;
+
+}
+.header_icon__line--top {
+  top: 20%;
+  width: 40%;
+}
+.header_icon__line--middle {
+  top: 50%;
+  width: 80%;
+}
+.header_icon__line--bottom {
+  top: 80%;
+  width: 20%;
+}
+.header_icon--text {
+  position: relative;
+  left: 40px;
+  top: 20px;
+  color: blue;
+  font-weight: bold;
+
+}
+</style>
---
+++ pages/thanks.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="thanks__container">
+    <Header />
+    <div class="thanks__sub_container">
+      <p class="thanks__text">会員登録ありがとうございます</p>
+      <a href="/login" class="login_btn">ログインする</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.thanks__container {
+background-color: gainsboro;
+width: 100vw;
+height: 100vh;
+}
+
+.thanks__sub_container {
+  width: 50%;
+  margin: 100px auto 0;
+  text-align: center;
+  padding: 60px;
+  box-shadow: 2px 2px 2px grey;
+  background-color: white;
+}
+
+.thanks__text {
+  font-size: 24px;
+  margin-bottom: 40px;
+}
+
+.login_btn {
+  text-decoration: none;
+  background-color: blue;
+  color: white;
+  padding: 10px;
+  border-radius: 4px;
+  margin-top: 30px;
+}
+
+</style>
